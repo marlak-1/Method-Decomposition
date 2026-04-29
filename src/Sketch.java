9696d4ad4eb1d25e42f624b65fd3dc86a3dcf5de
marlak-1 <marla.kamil27@ycdsbk12.ca>
2026-04-29 12:39:50 -0400

    Add improved maple leaf design

--- src/Sketch.java
+++ src/Sketch.java
@@ -30,12 +30,9 @@ public class Sketch extends PApplet {
 
         } else {
             drawCanadaStripes();
-            drawMapleLeaf();
+            drawMapleLeaf(width / 2, height / 2, 0.35f);
         }
     }
-
-
-
 
     private void drawHorizontalFlag(int topR, int topG, int topB, 
                                     int bottomR, int bottomG, int bottomB) {
@@ -55,20 +52,45 @@ public class Sketch extends PApplet {
         rect(width / 4, 0, width / 2, height);
     }
 
-    private void drawMapleLeaf() {
-        // Simplified star shape in place of maple leaf
+    /**
+     * Draws an accurate maple leaf centred at (cx, cy) with a scale factor.
+     * Vertices traced from a reference image of the Canadian flag.
+     *
+     * @param cx  centre x-position
+     * @param cy  centre y-position
+     * @param s   scale factor (1.0 = original traced size)
+     */
+    private void drawMapleLeaf(float cx, float cy, float s) {
+        final float CEN_X = 300;
+        final float CEN_Y = 330;
+
         fill(255, 0, 0);
         beginShape();
-        vertex(width / 2, 80);
-        vertex(width / 2 - 20, 160);
-        vertex(width / 2 - 60, 160);
-        vertex(width / 2 - 30, 220);
-        vertex(width / 2 - 50, 320);
-        vertex(width / 2,      260);
-        vertex(width / 2 + 50, 320);
-        vertex(width / 2 + 30, 220);
-        vertex(width / 2 + 60, 160);
-        vertex(width / 2 + 20, 160);
+        vertex(cx + (302 - CEN_X) * s, cy + (6   - CEN_Y) * s);
+        vertex(cx + (362 - CEN_X) * s, cy + (111 - CEN_Y) * s);
+        vertex(cx + (420 - CEN_X) * s, cy + (87  - CEN_Y) * s);
+        vertex(cx + (387 - CEN_X) * s, cy + (267 - CEN_Y) * s);
+        vertex(cx + (474 - CEN_X) * s, cy + (180 - CEN_Y) * s);
+        vertex(cx + (488 - CEN_X) * s, cy + (227 - CEN_Y) * s);
+        vertex(cx + (580 - CEN_X) * s, cy + (207 - CEN_Y) * s);
+        vertex(cx + (552 - CEN_X) * s, cy + (311 - CEN_Y) * s);
+        vertex(cx + (590 - CEN_X) * s, cy + (332 - CEN_Y) * s);
+        vertex(cx + (444 - CEN_X) * s, cy + (457 - CEN_Y) * s);
+        vertex(cx + (461 - CEN_X) * s, cy + (511 - CEN_Y) * s);
+        vertex(cx + (312 - CEN_X) * s, cy + (496 - CEN_Y) * s);
+        vertex(cx + (312 - CEN_X) * s, cy + (656 - CEN_Y) * s);
+        vertex(cx + (290 - CEN_X) * s, cy + (657 - CEN_Y) * s);
+        vertex(cx + (294 - CEN_X) * s, cy + (495 - CEN_Y) * s);
+        vertex(cx + (148 - CEN_X) * s, cy + (513 - CEN_Y) * s);
+        vertex(cx + (162 - CEN_X) * s, cy + (462 - CEN_Y) * s);
+        vertex(cx + (15  - CEN_X) * s, cy + (331 - CEN_Y) * s);
+        vertex(cx + (54  - CEN_X) * s, cy + (315 - CEN_Y) * s);
+        vertex(cx + (27  - CEN_X) * s, cy + (206 - CEN_Y) * s);
+        vertex(cx + (116 - CEN_X) * s, cy + (226 - CEN_Y) * s);
+        vertex(cx + (132 - CEN_X) * s, cy + (177 - CEN_Y) * s);
+        vertex(cx + (216 - CEN_X) * s, cy + (265 - CEN_Y) * s);
+        vertex(cx + (191 - CEN_X) * s, cy + (87  - CEN_Y) * s);
+        vertex(cx + (246 - CEN_X) * s, cy + (114 - CEN_Y) * s);
         endShape(CLOSE);
     }
 }
